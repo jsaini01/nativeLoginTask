78326de4f5d136097166bbcf9a1190aeeab44d0f
Jaspreet Singh <jaspreetsingh@WILASSETA0126.local>
2022-11-04 19:20:01 +0530

    apihit

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,13 +1,41 @@
 import {View, Text} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from '../../components/Button';
+// import {useRoute} from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = ({navigation}) => {
-  const [navigations] = useState(navigation);
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  // const route = useRoute();
+  const [token, setToken] = useState();
+
+  const retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('token');
+      if (value !== null) {
+        // We have data!!
+        console.log('fffff', value);
+
+        setToken(value);
+      }
+    } catch (error) {
+      console.log('errorsdsd', error);
+    }
+  };
+
+  const Handlerbtn = () => {
+    navigation.navigate('Login');
+    AsyncStorage.removeItem('token');
+  };
+
   return (
     <View>
-      <Text>home</Text>
-      <Button text="goback" navigations={navigations} />
+      <Text>{token}</Text>
+
+      <Button text="goback" onPress={() => Handlerbtn()} />
     </View>
   );
 };
--- src/views/pages/Login.js
+++ src/views/pages/Login.js
@@ -1,41 +1,51 @@
-import {View, TextInput, StyleSheet, Text, Button, Alert} from 'react-native';
+import {View, TextInput, StyleSheet, Text, Alert} from 'react-native';
+
 import React, {useState} from 'react';
 import Btn from '../../components/Button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({navigation}) => {
   //   const [navigations] = useState(navigation);
-  const [userId, setUserId] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // const [userData, setUserData] = useState();
 
   const Api = 'https://reqres.in/api/login';
 
   const validEmail = new RegExp(
     '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$',
   );
-  const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+  const validPassword = new RegExp('^(?=.?[a-zA-Z])');
 
   const apiHandle = async () => {
-    try {
-      let response = await fetch(Api, {
-        method: 'GET',
-      });
+    console.log('email', email);
+    console.log('password', password);
+    let items = {email, password};
+    if (validEmail.test(email) && validPassword.test(password)) {
+      fetch(Api, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(items),
+      })
+        .then(res => res.json())
+        .then(res => {
+          navigation.navigate('Home');
+          const StoreData = async () => {
+            try {
+              await AsyncStorage.setItem('token', JSON.stringify(res));
+            } catch (error) {
+              console.log('err', error);
+            }
+          };
+          StoreData();
+        })
 
-      let res = await response.json();
-      console.log('response', response.status);
-      if (response.status === 200) {
-        console.log('res', JSON.stringify(res));
-      } else {
-        console.log('not registered');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleSubmit = () => {
-    if (validEmail.test(userId) && validPassword.test(password)) {
-      navigation.navigate('Home');
-      apiHandle();
+        .catch(err => {
+          console.log('err', err);
+        });
     } else {
       Alert.alert('not valid');
     }
@@ -47,8 +57,8 @@ const Login = ({navigation}) => {
         style={styles.input}
         placeholder="User id"
         cursorColor={'grey'}
-        onChangeText={text => setUserId(text)}
-        value={userId}
+        onChangeText={text => setEmail(text)}
+        value={email}
       />
       <TextInput
         style={styles.input}
@@ -59,7 +69,7 @@ const Login = ({navigation}) => {
         value={password}
       />
       <Text style={styles.forgot}>Forgot Password</Text>
-      <Btn text="Login" onPress={() => handleSubmit()} />
+      <Btn text="Login" onPress={() => apiHandle()} />
     </View>
   );
 };
